9adf6e5aed0d35737452e4cfdc08c7ae3b3283ac
Cliche818 <jeff.chun.yinli@gmail.com>
2019-06-16 21:14:26 -0400

    added second hackerrank test example and it works :)

--- elixir/ema_supercomputer/test/ema_supercomputer_test.exs
+++ elixir/ema_supercomputer/test/ema_supercomputer_test.exs
@@ -11,4 +11,15 @@ defmodule EmaSupercomputerTest do
         ["G", "G", "G", "G", "G", "G"]])
     assert actual == 5
   end
+
+  test "the returned value should be 25" do
+    actual = EmaSupercomputer.solve(
+      [["B", "G", "B", "B", "G", "B"],
+        ["G", "G", "G", "G", "G", "G"],
+        ["B", "G", "B", "B", "G", "B"],
+        ["G", "G", "G", "G", "G", "G"],
+        ["B", "G", "B", "B", "G", "B"],
+        ["B", "G", "B", "B", "G", "B"]])
+    assert actual == 25
+  end
 end
